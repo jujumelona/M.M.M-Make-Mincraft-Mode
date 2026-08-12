aafd03dd91d4e4f1900dc1d60f90ffc95568a996
jujumelona <qasdaa025@gmail.com>
2026-08-12 16:05:58 +0900

    Install planner pagination safety contract

--- minecraft_mod_ai/final_architecture_contract.py
+++ minecraft_mod_ai/final_architecture_contract.py
@@ -18,6 +18,7 @@ from .build_input_scope_contract import install as _install_build_input_scope
 from .clean_room_verification_contract import install as _install_clean_room
 from .custom_generation_search_contract import install as _install_custom_generation_search
 from .planner_json_runtime_contract import install as _install_planner_json_runtime
+from .planner_pagination_safety_contract import install as _install_planner_pagination_safety
 from .repair_diagnostics_contract import install as _install_repair_diagnostics
 from .repair_memory_budget_contract import install as _install_repair_memory_budget
 from .required_gate_compatibility_contract import install as _install_gate_compatibility
@@ -75,6 +76,7 @@ def install(
         validation_module,
     )
     _install_planner_json_runtime(complete_planner_module)
+    _install_planner_pagination_safety(complete_planner_module)
     _agentic_module.install(
         complete_planner_module=complete_planner_module,
         repair_module=repair_module,
