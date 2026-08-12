33bdfabbd3357ec2d89cbc904987804055ba5035
jujumelona <qasdaa025@gmail.com>
2026-08-12 15:42:39 +0900

    Install custom generation verifier search

--- minecraft_mod_ai/final_architecture_contract.py
+++ minecraft_mod_ai/final_architecture_contract.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from . import agentic_optimization_contract as _agentic_module
 from . import atomic_requirement_contract as _atomic_module
+from . import custom_module_generator as _custom_module_generator_module
 from . import production_contract as _production_contract_module
 from . import work_graph as _work_graph_module
 from .atomic_efficiency_contract import install as _install_atomic_efficiency
@@ -15,6 +16,7 @@ from .atomic_quality_binding_contract import install as _install_atomic_quality
 from .atomic_requirement_contract import install as _install_atomic_requirements
 from .build_input_scope_contract import install as _install_build_input_scope
 from .clean_room_verification_contract import install as _install_clean_room
+from .custom_generation_search_contract import install as _install_custom_generation_search
 from .planner_json_runtime_contract import install as _install_planner_json_runtime
 from .repair_diagnostics_contract import install as _install_repair_diagnostics
 from .repair_memory_budget_contract import install as _install_repair_memory_budget
@@ -78,6 +80,7 @@ def install(
         repair_module=repair_module,
         work_graph_module=_work_graph_module,
     )
+    _install_custom_generation_search(_custom_module_generator_module)
     _install_repair_memory_budget(_agentic_module)
     _install_scheduler_fairness(_work_graph_module)
     _install_visual_scope(orchestrator_module)
