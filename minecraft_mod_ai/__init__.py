c1a7af968e32bafed4965e829f232a1fbfe60bd9
jujumelona <qasdaa025@gmail.com>
2026-08-16 01:56:59 +0900

    Install production page progress contract

--- minecraft_mod_ai/__init__.py
+++ minecraft_mod_ai/__init__.py
@@ -4,6 +4,10 @@ from .runtime_bootstrap import initialize_runtime
 
 initialize_runtime()
 
+from .production_page_progress_contract import install as install_production_page_progress_contract
+
+install_production_page_progress_contract()
+
 from .api import (
     ChatReply,
     CompleteChatReply,
