876ae04f2a4fbd82a832519d76a239d60aaf255c
jujumelona <qasdaa025@gmail.com>
2026-08-17 19:47:47 +0900

    fix: restore runtime bootstrap ownership

--- minecraft_mod_ai/__init__.py
+++ minecraft_mod_ai/__init__.py
@@ -4,11 +4,6 @@ from .runtime_bootstrap import initialize_runtime
 
 initialize_runtime()
 
-from .runtime_regression_fixes import install as _install_runtime_regression_fixes
-
-_install_runtime_regression_fixes()
-del _install_runtime_regression_fixes
-
 from .api import (
     ChatReply,
     CompleteChatReply,
