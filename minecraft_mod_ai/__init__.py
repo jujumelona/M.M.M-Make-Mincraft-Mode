a2eb98f077051019c390a0ccedabe68992a40841
jujumelona <qasdaa025@gmail.com>
2026-08-29 20:52:51 +0900

    fix: install pack metadata ABI hardening

--- minecraft_mod_ai/__init__.py
+++ minecraft_mod_ai/__init__.py
@@ -8,9 +8,11 @@ finalize_runtime()
 
 from .pipeline_hardening import install_pipeline_hardening
 from .pipeline_hardening_v2 import install_pipeline_hardening_v2
+from .pipeline_hardening_v3 import install_pipeline_hardening_v3
 
 install_pipeline_hardening()
 install_pipeline_hardening_v2()
+install_pipeline_hardening_v3()
 
 from .api import (
     ChatReply,
