0854002a8129d0b0d4785f70b29f3df2801fe8f4
jujumelona <qasdaa025@gmail.com>
2026-09-09 22:58:09 +0900

    fix: compose verifier and fast-mode policies in runtime bootstrap

--- minecraft_mod_ai/runtime_bootstrap.py
+++ minecraft_mod_ai/runtime_bootstrap.py
@@ -106,13 +106,15 @@ def _install_model_runtime_contracts() -> None:
 
 
 def _install_validation_contracts() -> None:
-    from . import java_lsp, repair_engine, runner, validation_execution_contract
+    from . import java_lsp, production_tools, repair_engine, runner, validation_execution_contract
+    from .java_diagnostics_fallback_contract import install as install_java_diagnostics_fallback
     from .java_lsp_process_safety_contract import install as install_java_lsp_process_safety
     from .research_validation_fingerprint_performance import harden as harden_validation_fingerprints
     from .validation_execution_contract import install as install_validation_execution
 
     install_validation_execution(runner, java_lsp, repair_engine)
     install_java_lsp_process_safety(java_lsp)
+    install_java_diagnostics_fallback(production_tools.ProductionToolService)
     harden_validation_fingerprints(validation_execution_contract)
 
 
@@ -234,6 +236,7 @@ def _install_architecture_contracts() -> None:
     from .clean_room_verification_contract import install as install_clean_room
     from .coder_max_efficiency_contract import install_coder_max_efficiency
     from .custom_generation_search_contract import install as install_custom_generation_search
+    from .fast_mode_quality_contract_installation import install as install_fast_mode_quality_contract
     from .repair_diagnostics_contract import install as install_repair_diagnostics
     from .repair_memory_budget_contract import install as install_repair_memory_budget
     from .research_evidence_handoff_contract import install as install_research_evidence_handoff
@@ -262,6 +265,7 @@ def _install_architecture_contracts() -> None:
     )
     install_custom_generation_search(custom_module_generator)
     install_coder_max_efficiency()
+    install_fast_mode_quality_contract(custom_module_generator)
     install_repair_memory_budget(agentic_optimization_contract)
     install_visual_scope(complete_orchestrator)
     install_gate_compatibility(complete_orchestrator)
