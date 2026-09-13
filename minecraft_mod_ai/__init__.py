6e6961957688ec3f595cfa23ba66d0ae0e01c1cd
jujumelona <qasdaa025@gmail.com>
2026-09-13 20:01:27 +0900

    fix: restore package exports after bootstrap cleanup

--- minecraft_mod_ai/__init__.py
+++ minecraft_mod_ai/__init__.py
@@ -77,20 +77,62 @@ from .complete_orchestrator_support import CompleteProductionError
 from .complete_planner import CompleteGameDesignPlanner
 from .complete_spec import AssetRequest, CompleteProposal, ProductionModule
 from .ecosystem_discovery import EcosystemDiscoveryClient
+from .external_mcp import ExternalMCPRegistry
+from .importer import (
+    ExistingProjectImportError,
+    ExistingProjectReport,
+    inspect_existing_project_archive,
+)
+from .mod_development_methods import (
+    ModDevelopmentMethod,
+    mod_development_method_catalog,
+    resolve_mod_development_methods,
+)
+from .model_adapters import ModelBackendError, ModelConfigurationError
+from .model_registry import ModelRegistry
+from .model_router import ModelRouter
+from .pipeline import PipelineResult
+from .planner import HeuristicPlanner, OpenAICompatiblePlanner
+from .production_contract import (
+    ProductionContractCompilation,
+    compile_production_contract,
+    evaluate_quality_contract,
+    quality_contract_summary,
+    quality_unresolved,
+)
+from .production_tools import ProductionToolService
+from .project_index import ProjectIndex
+from .rag_index import ProjectRAGIndex
+from .routed_planner import RoutedPlanner
+from .scalable_generator import ScalableFabricProjectGenerator
+from .scalable_pipeline import ScalableMinecraftModPipeline
+from .scale_policy import ScalePolicy, ScalePolicyError
+from .spec import BossSpec, ContentSpec, ModSpec, PlatformLock, Proposal
+from .technology_radar import (
+    assess_technology_compatibility,
+    build_technology_radar,
+    technology_research_routes,
+)
+
+install_source_observation_budget()
+
+MinecraftModPipeline = ScalableMinecraftModPipeline
 
 __all__ = [
-    "AssetRequest",
-    "ChatReply",
-    "CompleteChatReply",
-    "CompleteExecutionOptions",
-    "CompleteGameDesignPlanner",
-    "CompleteModAISession",
-    "CompletePipelineResult",
-    "CompleteProductionError",
-    "CompleteProductionOrchestrator",
-    "CompleteProposal",
-    "EcosystemDiscoveryClient",
-    "ModAISession",
-    "ProductionModule",
-    "supported_minecraft_versions",
+    "AssetRequest", "BossSpec", "ChatReply", "CompleteChatReply",
+    "CompleteExecutionOptions", "CompleteGameDesignPlanner", "CompleteModAISession",
+    "CompletePipelineResult", "CompleteProductionError", "CompleteProductionOrchestrator",
+    "CompleteProposal", "ContentSpec", "EcosystemDiscoveryClient", "ExistingProjectImportError",
+    "ExistingProjectReport", "ExternalMCPRegistry", "HeuristicPlanner", "MinecraftModPipeline",
+    "ModAISession", "ModDevelopmentMethod", "ModSpec", "ModelBackendError",
+    "ModelConfigurationError", "ModelRegistry", "ModelRouter", "OpenAICompatiblePlanner",
+    "PipelineResult", "PlatformLock", "ProductionContractCompilation", "ProductionModule",
+    "ProductionToolService", "ProjectIndex", "ProjectRAGIndex", "Proposal", "RoutedPlanner",
+    "ScalableFabricProjectGenerator", "ScalableMinecraftModPipeline", "ScalePolicy",
+    "ScalePolicyError", "assess_technology_compatibility", "build_technology_radar",
+    "compile_production_contract", "evaluate_quality_contract", "inspect_existing_project_archive",
+    "mod_development_method_catalog", "quality_contract_summary", "quality_unresolved",
+    "resolve_mod_development_methods", "supported_minecraft_versions", "technology_research_routes",
 ]
+
+__version__ = "0.8.0"
