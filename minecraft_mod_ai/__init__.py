e5112c738c5f4e6b219cb06500de89a27b025110
jujumelona <qasdaa025@gmail.com>
2026-09-26 04:14:29 +0900

    Install runtime contracts at package bootstrap

--- minecraft_mod_ai/__init__.py
+++ minecraft_mod_ai/__init__.py
@@ -55,6 +55,10 @@ from .technology_radar import (
 )
 
 
+from .runtime_bootstrap import bootstrap_runtime
+
+bootstrap_runtime()
+
 MinecraftModPipeline = ScalableMinecraftModPipeline
 
 __all__ = [
