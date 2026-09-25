ad461f62e285c74096bdea7f0bdeabcfe700bee0
jujumelona <qasdaa025@gmail.com>
2026-09-26 04:06:18 +0900

    Remove deleted runtime composer dependency

--- minecraft_mod_ai/adaptive_retrieval_contract.py
+++ minecraft_mod_ai/adaptive_retrieval_contract.py
@@ -17,11 +17,9 @@ def install(model_router_module: Any) -> None:
     from .agent_tool_allowlist_hardening import harden_agent_tool_allowlist
     from .hybrid_route_hardening import harden_code_search_routes
     from .inference_time_scaling import harden_runtime
-    from .runtime_composer_hardening import harden_runtime_composer_identity
 
     harden_runtime()
     harden_code_search_routes()
-    harden_runtime_composer_identity()
     harden_agent_tool_allowlist()
     _expose_composed_repair_contracts()
 
